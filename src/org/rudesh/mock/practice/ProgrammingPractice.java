8ab17aa41724d34c846e51c783ffe830fb63ca9b
RudeshV <vrudesh01@outlook.com>
2021-03-24 15:42:47 +0530

    Push test

---
+++ src/org/rudesh/mock/practice/ProgrammingPractice.java
@@ -0,0 +1,16 @@
+package org.rudesh.mock.practice;
+
+public class ProgrammingPractice {
+
+public static void main(String[] args) {
+	int n=1234,res=0,rem=0;
+
+	while (n>0) {
+		rem=n%10;    
+		res=(res*10)+rem;
+		n=n/10;
+	}
+	
+	
+}
+}
